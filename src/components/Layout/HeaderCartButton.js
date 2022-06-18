bcb5e2b92171d823555b2104dd878c7b78d17778
rawwr <akshayace0302@gmail.com>
2022-06-19 00:12:20 +0530

    using the context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,24 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cartContext";
 import classes from "./HeaderCartButton.module.css";
+
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  // the easiest-to-understand case for reduce() is to return the sum of all the elements in an array:
+  const initialValue = 0;
+
+  const numberOfCartItems = cartCtx.items.reduce((curNum, item) => {
+    return curNum + item.amount;
+  }, initialValue);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
