94be431da22731da50fdb0664e457351fc3f5a05
rawwr <akshayace0302@gmail.com>
2022-06-19 16:14:53 +0530

    working with refs and forward refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,22 @@
+import { useContext } from "react";
 import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cartContext";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   //to render two decimal places an a dollar sign in a dynamic price
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +25,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
